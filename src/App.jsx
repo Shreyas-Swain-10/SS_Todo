e6140806ff2935afae42ef25f47374d313f5a54f
Shreyas Swain <shreyas.swain07@gmail.com>
2024-08-30 23:25:15 +0530

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
 import './App.css'
-import { Todos } from './pages/Todos'
-
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+// import { AuthProvider } from './state/authContext'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Todos from './pages/Todos'
+// import PrivateRoute from './routes/privateRoute'
 function App() {
 
   return (
-    <>
-      <Todos />
-    </>
+    <AuthProvider>
+        <Router>
+          <Container maxW="container.md" py={6}>
+            <Box textAlign="center" mb={6}>
+              <Heading as="h1" size="xl">Todo Application</Heading>
+            </Box>
+            <Routes>
+              <Route path="/login" element={<SignIn />} />
+              {/* <Route path="/logout" element={<SignOut />} /> */}
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/todos/:userId" element={<PrivateRoute element={<Todos />} />} />
+              <Route path="/" element={<Navigate to="/login" />} />
+            </Routes>
+          </Container>
+        </Router>
+      </AuthProvider>
   )
 }
 
